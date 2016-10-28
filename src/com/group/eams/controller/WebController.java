c44860c417906f600e8e8a503d1093b8b57b2190
zcy-fover <15207189058@163.com>
2016-10-28 22:44:48 +0800

    框架完善，可以使用idea开发

--- src/com/group/eams/controller/WebController.java
+++ src/com/group/eams/controller/WebController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping
 public class WebController {
+	@RequestMapping("/")
 	public String forwardView(){
 		System.out.println("shuchu ceshi ");
 		return "index";
